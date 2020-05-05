6f4e24fcaf0d8c2ad752d7495f37073fb284083c
collactionofc <59391243+collactionofc@users.noreply.github.com>
2020-05-05 14:40:10 +0530

    added firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
+  firebase:{
   apiKey: "AIzaSyDgSEQNTqcdXmVN9VolUe7BDnVO2abbufc",
       authDomain: "chatroom-e8505.firebaseapp.com",
       databaseURL: "https://chatroom-e8505.firebaseio.com",
@@ -8,4 +9,5 @@ export const environment = {
       messagingSenderId: "680329547714",
       appId: "1:680329547714:web:a4504fccceb9dd27c3905b",
       measurementId: "G-X0Y82V8FBE"
+  }
 };
