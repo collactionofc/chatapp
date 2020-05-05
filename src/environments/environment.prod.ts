67f00d3434fd2b323f9ff97a36ede0b6c83b9da2
Bhavin Sen <bs.globalia@gmail.com>
2020-05-05 12:59:25 +0530

    resolved isssue

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: "AIzaSyDgSEQNTqcdXmVN9VolUe7BDnVO2abbufc",
+      authDomain: "chatroom-e8505.firebaseapp.com",
+      databaseURL: "https://chatroom-e8505.firebaseio.com",
+      projectId: "chatroom-e8505",
+      storageBucket: "chatroom-e8505.appspot.com",
+      messagingSenderId: "680329547714",
+      appId: "1:680329547714:web:a4504fccceb9dd27c3905b",
+      measurementId: "G-X0Y82V8FBE"
 };
